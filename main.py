7003de513da0bb31f3d1b40528aaf76466c342ac
hydeh3r3 <alituncgenc@gmail.com>
2024-09-21 19:29:29 +0300

    rotation

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from player import Player  # Add this import
-
-# Define BLACK if it's not in constants.py
-BLACK = (0, 0, 0)
-
-print("Starting asteroids!")
-print(f'Screen width: {SCREEN_WIDTH}')
-print(f'Screen height: {SCREEN_HEIGHT}')
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, BLACK
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
     
-    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)  # Move this line outside the loop
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
     
     running = True
     while running:
+        dt = clock.tick(60) / 1000
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -29,7 +23,6 @@ def main():
         player.draw(screen)
         
         pygame.display.flip()
-        dt = clock.tick(60) / 1000  # Combine these two lines
     
     pygame.quit()
 
--- player.py
+++ player.py
@@ -1,13 +1,24 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, WHITE
+from constants import PLAYER_RADIUS, WHITE, PLAYER_TURN_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y, radius=PLAYER_RADIUS):
         super().__init__(x, y, radius)
         self.thrust = 0
         self.rotation = 0
-        self.rotation_speed = 0
+        self.player_turn_speed = PLAYER_TURN_SPEED
+
+    def rotate(self, dt):
+        self.rotation += self.player_turn_speed * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt)
+        if keys[pygame.K_d]:
+            self.rotate(-dt)
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)

f433c44b26e735f6efca86b1ccbf82b1d66c634a
hydeh3r3 <alituncgenc@gmail.com>
2024-09-21 18:41:49 +0300

    3.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
+from constants.py import *
 
 print("Starting asteroids!")
+print('Screen width: {SCREEN_WIDTH}')
+print('Screen height: {SCREEN_HEIGHT}')
 
 if __name__ == "__main__":
     main()
